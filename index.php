683acf00aaede010f907778faf37cf5b1517418d
pavanghuge70 <79075068+pavanghuge70@users.noreply.github.com>
2021-02-23 23:53:29 +0530

    commit

--- index.php
+++ index.php
@@ -140,7 +140,7 @@
 
     <button onclick="document.getElementById('id01').style.display='block'" style="width: auto;">Login</button>
     <div id="id01" class="modal">
-        <form  action="login.php" method="POST" class="modal-content animate">
+        <form  action="" method="POST" class="modal-content animate">
             <div class="imgcontainer">
                 <span onclick="document.getElementById('id01').style.display='none'"
                 class="close" title="Close Modal">&times;</span>
@@ -166,3 +166,57 @@
     </div>
 </body>
 </html>
+
+<?php
+    $host = 'localhost';
+    $username = 'root';
+    $password = 'pavan07254';
+    $dbname = 'student_login';
+
+    //creat connection
+
+    $conn = mysqli_connect($host,$username,$password,$dbname);
+
+    //check connection 
+
+    if(mysqli_connect_errno())
+    {
+        echo 'fail to connect';
+        exit();
+    }
+    echo "conaction sucsseful";
+
+    // tack input from user
+
+    $uname = $_POST['username'];
+    $pass = $_POST['password'];
+
+    //write a query for login
+
+    $sql = "SELECT * FROM student_info WHERE name='$uname' and pass = '$pass'";
+
+    if(!mysqli_query($conn , $sql))
+    {
+        echo 'unalbe to login';
+    }
+    $result = mysqli_query($conn,$sql);
+
+    $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+    //$active = $row['active'];
+    
+    $count = mysqli_num_rows($result);
+    
+    
+    if($count == 1)
+    {
+       // session_register("name");
+        //$_SESSION['login_user']= $uname;
+        echo '<br> welcom';
+        header("location: main_page.html");
+    }
+    else {
+        $error = " Your Login Name or Password is invalid";
+        echo "<br>plz check";
+    }
+    
+?>
